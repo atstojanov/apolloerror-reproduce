06121a9d655512d4ac5bb66ba07577d105a46ca0
Angel Stoyanov <atstojanov@gmail.com>
2021-07-16 16:05:10 +0300

    Example client and server

---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+function App() {
+  const { data, error } = useQuery(
+    gql`
+      query {
+        echoOdd(odd: 2)
+      }
+    `
+  );
+
+  return (
+    <div className="App">
+      <div>Netowrk error: </div>
+      <div>{error?.networkError?.message}</div>
+      <br />
+      <div>GraphQLErrors:</div>
+      <ul>
+        {error?.graphQLErrors.map((e, i) => (
+          <li key={i}>{e.message}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  ApolloProvider,
+  from,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  console.log("Network Error: ", networkError);
+  console.log("GraphqlErrors: ", graphQLErrors);
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000",
+});
+
+const client = new ApolloClient({
+  link: from([errorLink, httpLink]),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
